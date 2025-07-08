b18b9a59febc6ab5a67490ff237c69ed6dd6dc3f
Закарян Станислав <stas.zakaryan@gmail.com>
2025-07-08 11:46:31 +0300

    refactor: move configuration(JsonSerializer,Swagger) from Program to Configurations/*

--- AdBoard.API/AdBoard.API/Program.cs
+++ AdBoard.API/AdBoard.API/Program.cs
@@ -1,4 +1,5 @@
 
+using AdBoard.API.Configurations;
 using AdBoard.DAL;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
@@ -12,23 +13,18 @@ namespace AdBoard.API
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllers()
-                .AddJsonOptions(options =>
-                {
-                    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-                });
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-            });
-            builder.Services.AddDbContext<AdBoardDbContext>();
+                .AddCustomJsonSerializerOptions();
 
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen()
+                .AddCustomSwaggerOptions();
+
+            builder.Services.AddDbContext<AdBoardDbContext>();
             var app = builder.Build();
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
+                app.UseSwagger(); 
                 app.UseSwaggerUI();
             }
 
